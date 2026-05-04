2a5e766fd198fdc764075419139743d8ef59cf3f
Claudia Kinuthia <claudiawkinuthia@gmail.com>
2026-05-04 12:40:11 +0300

    Add custom adapters
    Closes #7

--- src/hooks/view_transactions.ts
+++ src/hooks/view_transactions.ts
@@ -2,7 +2,16 @@ import { useState, useEffect, useCallback } from "react";
 import { Transaction } from "../models/transaction";
 import { fetchFromApi } from "../api/client";
 import { mapJsonPlaceholder } from "../adapters/jsonplaceholder_adapter";
+import { mapDummyJson } from "../adapters/dummyjson_adapter";
 import { ApiSource } from "../api/endpoints";
+import { mapFakestore, mapReqRes } from "../adapters";
+
+const adapters: Record<ApiSource, (data: any) => Transaction[]> = {
+  jsonplaceholder: mapJsonPlaceholder,
+  dummyjson: mapDummyJson,
+  fakestore: mapFakestore,
+  reqres: mapReqRes,
+};
 
 export const useTransactions = () => {
   const [transactions, setTransactions] = useState<Transaction[]>([]);
@@ -16,7 +25,8 @@ export const useTransactions = () => {
 
     try {
       const data = await fetchFromApi(source);
-      const normalized = mapJsonPlaceholder(data);
+      const adapter = adapters[source];
+      const normalized = adapter(data);
       setTransactions(normalized);
     } catch (err) {
       setError(err instanceof Error ? err.message : "Something went wrong");
